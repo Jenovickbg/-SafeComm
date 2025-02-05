6d24cb443afaac77779a061b822be309e92c26ef
jenovic <jenovickbg5@gmail.com>
2025-02-05 22:51:39 +0100

    4 commit

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import mysql.connector
 # Connexion à la base de données
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
+        host="102.223.130.118",
         user="root",  # Modifier avec ton utilisateur MySQL
         password="",  # Ajouter ton mot de passe MySQL
         database="messaging_app"
