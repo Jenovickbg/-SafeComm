54a79c017202247020586491b955fdaf1585eacd
jenovic <jenovickbg5@gmail.com>
2025-02-06 10:28:29 +0100

    6 commit

--- database.py
+++ database.py
@@ -3,8 +3,17 @@ import mysql.connector
 # Connexion à la base de données
 def get_db_connection():
     return mysql.connector.connect(
-        host="http://172.20.10.4/",
-        user="root",  # Modifier avec ton utilisateur MySQL
-        password="",  # Ajouter ton mot de passe MySQL
+        host="102.223.130.118",  # Sans "http://"
+        user="root",  # Modifier avec votre utilisateur MySQL
+        password="",  # Ajouter votre mot de passe MySQL
         database="messaging_app"
     )
+
+# Tester la connexion
+try:
+    conn = get_db_connection()
+    print("Connexion réussie à la base de données !")
+    conn.close()
+except mysql.connector.Error as err:
+    print(f"Erreur de connexion : {err}")
+
